8aba9c5507048daec2204b3efa9a5a5f2f90d1c3
Wwle <weiyuecn5@gmail.com>
2019-10-08 14:21:38 +0800

    10.8日更新

--- first.py
+++ first.py
@@ -31,14 +31,29 @@ def upshuju():
             print(data[0],data[1],data[2],data[3],data[4],data[6])
             c.execute("SELECT 宠物 FROM pad_shujuku WHERE 账号编号='%s'"%(data[1]))
             ycw=c.fetchone()
-            # print(ycw[0])
-            xcw=ycw[0]+data[4]+','
-            sql = "UPDATE pad_shujuku set 石头数量 = '%s',等级='%s',宠物='%s',更新时间='%s' where 账号编号='%s'"%(data[2],data[3],xcw,data[6],data[1])
-            c.execute(sql)
-            c.execute("UPDATE pad_huancun set 是否上传 = '0' where 唯一键='%s'"%(data[0]))
+            # print(ycw)
+            if 699<int(data[2])<800:
+                stsl=data[2].replace('7','2',1)
+            else:
+                stsl=data[2]    #石头数量处理,把7 改成 2
+            if ycw==None:
+                xcw = data[4] + ','
+                sql="INSERT INTO pad_shujuku (账号编号,石头数量,等级,宠物,更新时间,买家,价格,已卖) VALUES ('%s','%s','%s','%s','%s',' ',' ','0')"%(data[1],stsl,data[3],xcw,data[6])
+                c.execute(sql)
+                c.execute("UPDATE pad_huancun set 是否上传 = '0' where 唯一键='%s'" % (data[0]))
+            else:
+                xcw = ycw[0] + data[4] + ','
+                sql = "UPDATE pad_shujuku set 石头数量 = '%s',等级='%s',宠物='%s',更新时间='%s' where 账号编号='%s'"%(stsl,data[3],xcw,data[6],data[1])
+                c.execute(sql)
+                c.execute("UPDATE pad_huancun set 是否上传 = '0' where 唯一键='%s'"%(data[0]))
+            conn.commit()
         # break
-        time.sleep(0.1)
-    conn.commit()
+        # time.sleep(0.05)
     conn.close()
 
+
+
+#更新数据
+upshuju()
+#清除已卖或者被封
 isbuy()
